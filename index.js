aad3c4d63adc25059054a69bd9da053f4c19933a
_meugen_ <eugen35@mail.ru>
2017-12-07 20:12:45 +0500

    Async callback test added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import {sum} from "./src/helpers";
+import {sum, sumAsync} from "./src/helpers";
 
 console.log('Start counting');
 const s=sum(2,3);
 console.log(s);
+
+const sumAsyncResult=sumAsync(2,3,(err,result) => {
+  console.log('sumAsyncResult=');
+  console.log(result);
+});
