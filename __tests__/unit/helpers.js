5405d571df421f5ede2d76e28a6c41cc0ebe1eba
_meugen_ <eugen35@mail.ru>
2017-12-09 13:24:47 +0500

    Готовы асинхронные тесты

--- __tests__/unit/helpers.js
+++ __tests__/unit/helpers.js
@@ -1,7 +1,4 @@
-//import Promise from "bluebird";
-//import {sum, sumAsync} from "../../src/helpers";
-import {sum} from "../../src/helpers";
-
+import {sum, sumAsync, promisifiedSumAsync} from "../../src/helpers";
 
 test('sum',() => {
   debugger;
@@ -10,19 +7,11 @@ test('sum',() => {
   expect(s).toBe(7);
 });
 
-
-//const sumPromise = Promise.promisify(sumAsync);
-
-//test('sum',() => expect(sum(3,4)).toBe(7));
-
-/*
-test.skip('promised sum',() => sumPromise(3,4).then(res => expect(res).toBe(7)));
-
-test.skip('callbacked sum', done => {
+test('callbacked sum', done => {
   sumAsync(3,4,(err, res) => {
-    expect(res).toBe(7);
+    expect(res).toBe(8);
     done();
   });
 });
-*/
 
+test('promised sum',() => promisifiedSumAsync(3,4).then(res => expect(res).toBe(8)));
--- test/helpers.js
+++ test/helpers.js
@@ -1,27 +1,29 @@
-//import Promise from "bluebird";
-//import {sum, sumAsync} from "../../src/helpers";
-import {sum} from "../src/helpers";
+import {sum, sumAsync, promisifiedSumAsync} from "../src/helpers";
 const assert = require('assert');
 
+describe("something", function () {
+  this.timeout(15000);
 
-it('sum',() => {
-  const s = sum(3,4);
-  assert.equal(7, s);
-});
-
-
-//const sumPromise = Promise.promisify(sumAsync);
-
-//test('sum',() => expect(sum(3,4)).toBe(7));
-
-/*
-test.skip('promised sum',() => sumPromise(3,4).then(res => expect(res).toBe(7)));
-
-test.skip('callbacked sum', done => {
-  sumAsync(3,4,(err, res) => {
-    expect(res).toBe(7);
-    done();
+  it('sum',() => {
+    const s = sum(3,4);
+    assert.equal(7, s);
   });
+
+  it('sumAsync', done => {
+    sumAsync(3,4,(err, res) => {
+      assert.equal(8, res);
+      done();
+    });
+  });
+
+  it('promisifiedSumAsync', () => {
+    return promisifiedSumAsync(3,4).then(res => {
+      return assert.equal(8, res);
+    });
+  });
+
 });
-*/
+
+
+
 
